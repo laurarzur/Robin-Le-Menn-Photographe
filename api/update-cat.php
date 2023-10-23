f91bec82d85ce9489f284af57ee709698444f9d1
Laura <130566790+laurarzur@users.noreply.github.com>
2023-10-23 19:19:56 +0200

    Update update-cat.php

--- api/update-cat.php
+++ api/update-cat.php
@@ -2,7 +2,7 @@
     $id = $_COOKIE["idCategorie"]; 
     $nom = $_COOKIE["nouveauNom"]; 
 
-    $pdo = new PDO("mysql:host=localhost;port=3306;dbname=robin;charset=utf8", "laura", "lauraB@boudou22"); 
+    $pdo = new PDO(""); 
     if (isset($id) && isset($nom)) {
         $query = $pdo->prepare("UPDATE categories SET nom = :nom WHERE id = :id"); 
         require "../sanitize.php";
